7cca87b8be191b09ebe013a0e346434801b26ce8
NiallColman49 <na.design@live.co.uk>
2022-07-03 20:06:24 +0100

    images and click events updated

--- changeimage.js
+++ changeimage.js
@@ -1,22 +1,20 @@
-const bodyTag = document.querySelector('body');
+const bodyTag = document.querySelector("body");
 
 let imageGallery = [
-    'url(williams-joinery-2---legacy.jpg)', 
-    'url(williams-joinery-3---legacy.jpg)',
-    'url(williams-joinery-4---legacy.jpg)',
-    'url(williams-joinery-5---legacy.jpg)',
-    'url(williams-joinery-6---legacy.jpg)',
-    'url(williams-joinery-7---legacy.jpg)',
-    'url(williams-joinery-8---legacy.jpg)',
-    'url(williams-joinery-9---legacy.jpg)',
-    'url(williams-joinery-10---legacy.jpg)',
-    'url(williams-joinery-14---legacy.jpg)',
-    'url(williams-joinery-12---legacy.jpg)',
-    'url(williams-joinery-13---legacy.jpg)',
+  "url(williams-joinery-2---legacy.jpg)",
+  "url(williams-joinery-3---legacy.jpg)",
+  "url(williams-joinery-4---legacy.jpg)",
+  "url(williams-joinery-6---legacy.jpg)",
+  "url(williams-joinery-7---legacy.jpg)",
+  "url(williams-joinery-9---legacy.jpg)",
+  "url(williams-joinery-10---legacy.jpg)",
+  "url(williams-joinery-14---legacy.jpg)",
+  "url(williams-joinery-12---legacy.jpg)",
+  "url(williams-joinery-8---legacy.jpg)",
 ];
 
-let counter = 0; 
+let counter = 0;
 
-bodyTag.addEventListener('click', function (event) {
-    bodyTag.style.backgroundImage = imageGallery[counter ++];
- });
+bodyTag.addEventListener("click", function (event) {
+  bodyTag.style.backgroundImage = imageGallery[counter++];
+});
